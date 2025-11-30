776750cbea1c8658c2548d08a06550c3cf68359c
tlljyang <125533600+tlljyang@users.noreply.github.com>
2025-11-30 21:38:46 +0800

    fix

--- Project Frontend/叫号系统/index.php
+++ Project Frontend/叫号系统/index.php
@@ -55,7 +55,7 @@ if ($show_notification) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Calling System</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="./css/style.css">
 </head>
 <body>
     <div class="container">
